3d70550ffaa4efce9f86a4120cf84867547de161
AlexNorm00 <lexa_kel@mail.ru>
2022-01-22 12:21:42 +0300

    new Homeworks

---
+++ src/Homeworks/HomeWork_2.java
@@ -0,0 +1,29 @@
+package Homeworks;
+
+import java.util.Scanner;
+
+public class HomeWork_2 {
+    public static void main(String[] args) {
+        //1
+//        for(int i=-3;i<=3;i++)
+//            System.out.println("число: "+i);
+        //2
+//        for(int i=-3;i<=3;i--)
+//            System.out.println("число: " + i);
+        //3.1
+//        System.out.println("Введите число");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int sum = 0;
+//        for (int i = 1; i <= n; i++)
+//            sum += i;
+//        System.out.println("Ответ:"+sum);
+        //3.2
+//        System.out.println("Введите число");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int sum = n*(n+1)/2;
+//        System.out.println(sum);
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,40 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
+        System.out.println("Выберите номер ДЗ");
+        boolean s = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Homework_1;\n0. Выход.");
+        int i=-1;
+        while (s == false) {
+            i = sc.nextInt();
+            if (i!=0 || i!=1)
+            {
+                System.out.println("Недопустимое значение! Попробуйте ещё раз"+i);
+            }
+            else
+            {
+                if (i==1) System.out.println("Вы выбрали 1");
+                if (i==0) break;
+            }
+
+        }
 //        System.out.println("Введите число положительное или отрицательное");
 //        Scanner sc = new Scanner(System.in);
 //        String t=sc.nextInt() < 0 ? "отрицательное" : "положительное";
 //        System.out.println("Вы ввели "+ t + " число");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите 1ое число \n");
-        int a = sc.nextInt();
-        System.out.println("Введите 2ое число \n");
-        int b = sc.nextInt();
-        String otv =a!=0 && b!=0? "Ответ "+a/b : "Ошибка деления на 0";
-        System.out.println(otv);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введите 1ое число \n");
+//        int a = sc.nextInt();
+//        System.out.println("Введите 2ое число \n");
+//        int b = sc.nextInt();
+//        String otv =a!=0 && b!=0? "Ответ "+a/b : "Ошибка деления на 0";
+//        System.out.println(otv);
 
     }
-
 
 
 }
