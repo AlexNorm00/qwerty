f7a78ebf2f1846962f44c069c2686bf5e1486382
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-24 20:17:47 +0300

    Update Les_14.java

--- src/com/company/HomeWork/TwoWork/Les_14.java
+++ src/com/company/HomeWork/TwoWork/Les_14.java
@@ -1,11 +1,13 @@
 package com.company.HomeWork.TwoWork;
-
+//TEXT FORMATTING
+//+
 public class Les_14 {
     public static void main(String[] args) {
 //        Апишите программу печати таблицы перевода расстояний из дюймов в сантиметры для
 //        значений длинн от 1 до 20 дюймов. 1 дюйм = 2,54 см
         System.out.println("дюйм        см");
         final double inch=2.54;
+        //naming could be better
         double inchAll=0;
         for (int i=1;i<=20;i++)
         {
