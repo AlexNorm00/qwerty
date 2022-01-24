44e9b1f7348777af2018433630be875f7d3e44a6
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-24 20:13:16 +0300

    Update Les_9.java

--- src/com/company/HomeWork/TwoWork/Les_9.java
+++ src/com/company/HomeWork/TwoWork/Les_9.java
@@ -1,11 +1,13 @@
 package com.company.HomeWork.TwoWork;
-
+//TEXT FORMATTING
+//+
 public class Les_9 {
     public static void main(String[] args) {
 //         2) Напишите программу вывода всех чётных чисел от 2 до 100 включительно
         int i=1;
         while (i<=100)
         {
+            //after if(...) use {} please 
             if (i%2==0) System.out.println(i);
             i++;
         }
