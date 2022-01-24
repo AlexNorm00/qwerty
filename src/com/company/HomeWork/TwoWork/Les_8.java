bfb3c6cd6d2edc9cf9b934e56c815f1a803ce085
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-24 20:12:02 +0300

    Update Les_8.java

--- src/com/company/HomeWork/TwoWork/Les_8.java
+++ src/com/company/HomeWork/TwoWork/Les_8.java
@@ -1,5 +1,5 @@
 package com.company.HomeWork.TwoWork;
-
+//TEXT FORMATTING!
 public class Les_8 {
     public static void main(String[] args) {
 //        1) while Вычислить: 1+2+4+8+...+256
@@ -8,6 +8,7 @@ public class Les_8 {
         while (i<=256)
         {
             sum+=i;
+            //why do you need to write i?
             System.out.println(sum+" "+i);
             i*=2;
         }
