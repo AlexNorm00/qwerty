60d0e01e3932b0c655ba88297b3042fdd9dea545
AlexNorm00 <lexa_kel@mail.ru>
2022-02-05 12:16:54 +0300

    Struct to Control work one

---
+++ src/com/company/HomeWork/FourWork/les_21.java
@@ -0,0 +1,32 @@
+package com.company.HomeWork.FourWork;
+
+import java.util.Scanner;
+
+public class les_21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //1,2,2,3,4
+        System.out.print("Введите длинну массива: ");
+        int[] mas = new int[scanner.nextInt()];
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print("Введите число №" + i + ": ");
+            mas[i] = scanner.nextInt();
+            //    if(mas[i]%2!=0) mas[i]=0;
+        }
+        System.out.println("Введите число для сравнения");
+        int Dodo = scanner.nextInt();
+        int j = 0;
+        while (j < mas.length) {
+            System.out.print(mas[j] == Dodo ? "Число присутвует на позиции " + j + "\n" : "");
+            j++;
+        }
+        double GetSum = 0;
+        for (int i : mas) {
+            GetSum += i;
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nСумма чисел массива ровна " + GetSum);
+        System.out.println("Среднее арифметическое " + GetSum / mas.length);
+    }
+}
