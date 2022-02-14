1c622f21db865e53009213f8483604e1284e2284
AlexNorm00 <lexa_kel@mail.ru>
2022-02-14 13:22:07 +0300

    Struct to Control work one

--- src/com/company/HomeWork/FiveWork/Les_34.java
+++ src/com/company/HomeWork/FiveWork/Les_34.java
@@ -8,20 +8,20 @@ public class Les_34 {
         Scanner scanner = new  Scanner(System.in);
         System.out.print("Бабочка:\nВведите длинну двумерного массива: ");
         int size = scanner.nextInt();
-        int[][] buterflayArray = new int[size][size];
+        int[][] array = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                buterflayArray[i][j] = j + 1;
-                if (j >= buterflayArray[j].length / 2)
-                    buterflayArray[i][j] = buterflayArray[j].length - buterflayArray[i][j] + 1;
-                if (buterflayArray[i][j] >= i + 2) //ВЫРЕЗАЕТ С 2ЙКИ первую половину
-                    buterflayArray[i][j] = -1;
-                if (i - 1 >= buterflayArray[i].length / 2) //вырезает вторую половину
-                    buterflayArray[i][j] = buterflayArray[buterflayArray[i].length - i - 1][j];
-                if (buterflayArray[i][j] != -1) {
-                    if (buterflayArray[i][j] < 10)
-                        System.out.print(" " + buterflayArray[i][j] + " ");
-                    else System.out.print(buterflayArray[i][j] + " ");
+                array[i][j] = j + 1;
+                if (j >= array[j].length / 2)
+                    array[i][j] = array[j].length - array[i][j] + 1;
+                if (array[i][j] >= i + 2) //ВЫРЕЗАЕТ С 2ЙКИ первую половину
+                    array[i][j] = -1;
+                if (i - 1 >= array[i].length / 2) //вырезает вторую половину
+                    array[i][j] = array[array[i].length - i - 1][j];
+                if (array[i][j] != -1) {
+                    if (array[i][j] < 10)
+                        System.out.print(" " + array[i][j] + " ");
+                    else System.out.print(array[i][j] + " ");
                 } else System.out.print("   ");
             }
             System.out.println();
