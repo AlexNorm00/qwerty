bf64eea565c091a0c920ea411da2ca1625776601
AlexNorm00 <lexa_kel@mail.ru>
2022-02-16 15:36:09 +0300

    Struct to Control work one

--- src/com/company/HomeWork/FourWork/Les_21.java
+++ src/com/company/HomeWork/FourWork/Les_21.java
@@ -6,7 +6,11 @@ public class Les_21 {
     public static void main(String[] args) {
         //Создать массив из 3 элементов. Вывести сумму.
         int[] array = {10, 13, 24};
-        for (int i = 0; i<array.length;i++)
-            System.out.print(array[i]+" ");
+        int sum=0;
+        for (int i = 0; i<array.length;i++) {
+            System.out.print(array[i] + " ");
+            sum+=array[i];
+        }
+        System.out.println("Сумма = "+sum);
     }
 }
